c67ba4beaaee7280400339fe78cbadd7bdb37b23
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-06-26 17:46:59 -0400

    feat: consult and update model

--- src/main/java/store/dao/ProductDAO.java
+++ src/main/java/store/dao/ProductDAO.java
@@ -4,6 +4,9 @@ import jakarta.persistence.EntityManager;
 import store.JPA;
 import store.entity.Product;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 
 public class ProductDAO {
     private EntityManager entityManager ;
@@ -17,8 +20,28 @@ public class ProductDAO {
     }
 
     private void ocultTransaction(){
-        entityManager.getTransaction().begin();
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        this.entityManager.getTransaction().begin();
+        this.entityManager.getTransaction().commit();
+        this.entityManager.close();
     }
+    public void update(Long id, BigDecimal price){
+        Product model = this.selectById(id);
+        model = this.entityManager.merge(model);
+        model.setPrice(price);
+        this.ocultTransaction();
+    }
+
+    public Product selectById(Long id){
+        return this.entityManager.find(Product.class, id);
+    }
+
+    public List<Product> select(){
+        return this.entityManager.createQuery("SELECT p FROM Product p").getResultList();
+    }
+
+    public List<Product> selectLessThan(BigDecimal value){
+        String jpql = "SELECT p FROM Product p WHERE p.price < :value";
+        return this.entityManager.createQuery(jpql).setParameter("value", value).getResultList();
+    }
+
 }
