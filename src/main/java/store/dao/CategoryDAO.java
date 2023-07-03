a6a7dd6446804b06bdb66deac32d1f341add2c9c
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-07-03 03:25:47 -0400

    feat: dao class to category managements

---
+++ src/main/java/store/dao/CategoryDAO.java
@@ -0,0 +1,33 @@
+package store.dao;
+
+import jakarta.persistence.EntityManager;
+import store.JPA;
+import store.entity.Category;
+
+import java.util.List;
+
+public class CategoryDAO {
+    private EntityManager entityManager;
+    public CategoryDAO(){
+        this.entityManager = JPA.returnEntityManager();
+    }
+
+    public void add(Category category){
+        this.entityManager.persist(category);
+        ocultTransaction();
+    }
+    private void ocultTransaction(){
+        this.entityManager.getTransaction().begin();
+        this.entityManager.getTransaction().commit();
+        this.entityManager.close();
+    }
+
+    public Category selectById(Long id){
+        return this.entityManager.find(Category.class, id);
+    }
+
+    public List<Category> selectAll(){
+        return this.entityManager.createQuery("SELECT c FROM Category c").getResultList();
+    }
+
+}
