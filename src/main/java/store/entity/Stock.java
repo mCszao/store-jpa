234c709f9a13490cd5e6235b96cc34ad9fbb0204
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-08-15 01:43:19 -0400

    feat: new stock entity to managed product and stocks

--- src/main/java/store/entity/Stock.java
+++ src/main/java/store/entity/Stock.java
@@ -1,2 +1,24 @@
-package store.entity;public class Stock {
+package store.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "stock")
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Product product;
+    @ManyToOne
+    private Stocks stock;
+    private BigDecimal product_quantity;
+
+    public Stock(Product product, Stocks stockOnStocks){
+        this.product = product;
+        this.stock = stockOnStocks;
+    }
 }
