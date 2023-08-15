a770f81104853b9786036286a4974d8781a632b2
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-08-15 00:02:35 -0400

    feat: new create table stock

--- src/main/resources/scripts/create.sql
+++ src/main/resources/scripts/create.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE store;
+use store;
 CREATE TABLE categories (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL
@@ -13,3 +14,21 @@ CREATE TABLE products (
   createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (categoryId) REFERENCES categories(id)
 );
+
+
+CREATE TABLE stocks (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE stock (
+  product_id INT NOT NULL,
+  product_stock INT NOT NULL,
+  product_quantity DECIMAL (3, 2) NOT NULL default 0,
+  FOREIGN KEY (product_id) REFERENCES products(id),
+  FOREIGN KEY (product_stock) REFERENCES stocks(id)
+);
+
+
+
+
