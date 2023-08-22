513bceef9eb6a023c8b8a9617afe76d7bae07218
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-08-21 22:21:12 -0400

    test main

--- src/main/java/TesteNewStock.java
+++ src/main/java/TesteNewStock.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class TesteNewStock {
+import jakarta.persistence.EntityManager;
+import store.JPA;
+import store.dao.CategoryDAO;
+import store.dao.ProductDAO;
+import store.dao.StockDAO;
+import store.dao.StocksDAO;
+import store.entity.Category;
+import store.entity.Product;
+import store.entity.Stock;
+import store.entity.Stocks;
+
+import java.math.BigDecimal;
+
+public class TesteNewStock {
+    public static void main(String[] args) {
+        EntityManager em = JPA.returnEntityManager();
+
+
+
+        Category categorie = new CategoryDAO(em).selectById(1L);
+
+        new ProductDAO(em).create(new Product("XIOMI", "preto", new BigDecimal("60"),categorie));
+
+        Product last = new ProductDAO(em).selectById(1L);
+        Stocks stocks = new StocksDAO(em).selectById(1L);
+        new StockDAO(em).add(new Stock(last, stocks));
+
+        em.getTransaction().begin();
+        em.getTransaction().commit();
+        em.close();
+
+
+    }
 }
--- src/main/java/store/dao/StocksDAO.java
+++ src/main/java/store/dao/StocksDAO.java
@@ -2,15 +2,23 @@ package store.dao;
 
 import jakarta.persistence.EntityManager;
 import store.JPA;
+import store.entity.Category;
 import store.entity.Stocks;
 
 public class StocksDAO extends MyDAO{
 
-
+    public StocksDAO(){
+        super();
+    }
+    public StocksDAO(EntityManager entityManager){
+        super(entityManager);
+    }
     public void add(Stocks stock){
         this.entityManager.persist(stock);
-        ocultTransaction();
     }
 
-
+    public Stocks selectById(Long id){
+        System.out.println(id);
+        return this.entityManager.find(Stocks.class, id);
+    }
 }
