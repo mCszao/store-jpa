82c0ff76e1c7ed80cc4bc8e3f23e1af3834f4463
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-08-21 22:20:35 -0400

    fix: removing occult uncontrolled transaction

--- src/main/java/store/dao/StockDAO.java
+++ src/main/java/store/dao/StockDAO.java
@@ -1,15 +1,20 @@
 package store.dao;
 
 import jakarta.persistence.EntityManager;
-import store.JPA;
 import store.entity.Stock;
 
 public class StockDAO extends MyDAO{
 
+    public StockDAO(){
+        super();
+    }
 
+    public StockDAO (EntityManager entityManager){
+        super(entityManager);
+    }
     public void add(Stock stock){
         this.entityManager.persist(stock);
-        ocultTransaction();
+
     }
 
 
