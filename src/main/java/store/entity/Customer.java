a3f4f7f0b326212ba76dcc07ed87bfaec93ce448
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-08-28 23:43:37 -0400

    feat: getters and setter

--- src/main/java/store/entity/Customer.java
+++ src/main/java/store/entity/Customer.java
@@ -20,4 +20,36 @@ public class Customer {
         this.email = email;
         this.phone = phone;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
