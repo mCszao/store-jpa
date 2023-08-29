a636f00b872a125193a819ca753bd33b545270a4
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-08-28 23:41:57 -0400

    feat: costumer attributes

--- src/main/java/store/entity/Customer.java
+++ src/main/java/store/entity/Customer.java
@@ -1,5 +1,16 @@
 package store.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String email;
+    private String phone;
 
 }
