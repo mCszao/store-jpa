8aef20eb89dad555ea5d94ad4de3c3ecdb555228
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-08-28 23:56:49 -0400

    feat: order attributes

--- src/main/java/store/entity/Order.java
+++ src/main/java/store/entity/Order.java
@@ -1,10 +1,16 @@
 package store.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "orders")
 public class Order {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal fullSaleValue;
+    private LocalDate createdAt = LocalDate.now();
 }
