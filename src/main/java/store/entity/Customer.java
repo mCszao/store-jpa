6a550f3d9b6fb0710ae72cc15a6a1ff40ad49c9a
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-08-29 01:09:21 -0400

    fix: not null constraint in customer model

--- src/main/java/store/entity/Customer.java
+++ src/main/java/store/entity/Customer.java
@@ -10,7 +10,9 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String cpf;
     private String email;
     private String phone;
