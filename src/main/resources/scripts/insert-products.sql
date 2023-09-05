545d04a339477ba30397548e6c46439f4376af4e
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-09-05 01:17:52 -0400

    feat: new sql script to insert products in database based if you run categories insert

---
+++ src/main/resources/scripts/insert-products.sql
@@ -0,0 +1,12 @@
+INSERT INTO products (createdAt, description, name, price, category_id)
+VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Headset Hero III', 200, 1);
+INSERT INTO products (createdAt, description, name, price, category_id)
+VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Mouse Logitech G209', 196, 1);
+INSERT INTO products (createdAt, description, name, price, category_id)
+VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Teclado Mecanico Reddragon', 180, 1);
+INSERT INTO products (createdAt, description, name, price, category_id)
+VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Suporte Corsair Headset', 369, 1);
+INSERT INTO products (createdAt, description, name, price, category_id)
+VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Monitor Dell P2217H', 1600, 1);
+INSERT INTO products (createdAt, description, name, price, category_id)
+VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Monitor Dell p1913', 800, 1);
