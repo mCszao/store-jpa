2d75cea4214308067768e03f52fe90e5f5bcaa66
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-09-05 01:25:57 -0400

    feat: refact in script insert products

--- src/main/resources/scripts/insert-products.sql
+++ src/main/resources/scripts/insert-products.sql
@@ -1,12 +1,7 @@
-INSERT INTO products (createdAt, description, name, price, category_id)
-VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Headset Hero III', 200, 1);
-INSERT INTO products (createdAt, description, name, price, category_id)
-VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Mouse Logitech G209', 196, 1);
-INSERT INTO products (createdAt, description, name, price, category_id)
-VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Teclado Mecanico Reddragon', 180, 1);
-INSERT INTO products (createdAt, description, name, price, category_id)
-VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Suporte Corsair Headset', 369, 1);
-INSERT INTO products (createdAt, description, name, price, category_id)
-VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Monitor Dell P2217H', 1600, 1);
-INSERT INTO products (createdAt, description, name, price, category_id)
-VALUES (now(),'Excelente aparelho para acompanhar nas gameplays','Monitor Dell p1913', 800, 1);
+INSERT INTO products (createdAt, description, name, price, category_id) VALUES
+('2023-09-05', 'Ótimo headset para jogadores de PC', 'Headset Gamer HyperX Cloud II', 199.99, 1),
+('2023-09-04', 'Mouse de alta precisão para jogos', 'Mouse Razer DeathAdder Elite', 79.99, 1),
+('2023-09-03', 'Teclado mecânico com iluminação RGB', 'Teclado Corsair K95 RGB Platinum', 229.99, 1),
+('2023-09-02', 'Suporte para headset resistente e elegante', 'Suporte para Headset NZXT Puck', 24.99, 1),
+('2023-09-01', 'Monitor gaming de alta resolução', 'Monitor ASUS ROG Swift PG279Q', 699.99, 1),
+('2023-08-31', 'Monitor de tela plana para uso geral', 'Monitor LG 24MK400H-B', 119.99, 1);
