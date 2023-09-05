f60f26ad740ed1e2a5ff01d9b55136862ce7c2f4
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-09-05 01:23:52 -0400

    feat: add new sql script to insert customers in database

---
+++ src/main/resources/scripts/insert-customers.sql
@@ -0,0 +1,11 @@
+INSERT INTO customers (cpf, createdAt, email, name, phone) VALUES
+('123.456.789-01', '2023-09-05', 'cliente1@email.com', 'Cliente 1', '+55 11 1234-5678'),
+('234.567.890-12', '2023-09-05', 'cliente2@email.com', 'Cliente 2', '+55 21 9876-5432'),
+('345.678.901-23', '2023-09-05', 'cliente3@email.com', 'Cliente 3', '+55 31 5555-5555'),
+('456.789.012-34', '2023-09-05', 'cliente4@email.com', 'Cliente 4', '+55 41 9999-9999'),
+('567.890.123-45', '2023-09-05', 'cliente5@email.com', 'Cliente 5', '+55 51 7777-7777'),
+('678.901.234-56', '2023-09-05', 'cliente6@email.com', 'Cliente 6', '+55 61 8888-8888'),
+('789.012.345-67', '2023-09-05', 'cliente7@email.com', 'Cliente 7', '+55 71 3333-3333'),
+('890.123.456-78', '2023-09-05', 'cliente8@email.com', 'Cliente 8', '+55 81 6666-6666'),
+('901.234.567-89', '2023-09-05', 'cliente9@email.com', 'Cliente 9', '+55 91 2222-2222'),
+('012.345.678-90', '2023-09-05', 'cliente10@email.com', 'Cliente 10', '+55 01 4444-4444');
